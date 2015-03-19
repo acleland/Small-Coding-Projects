ce35a2110744291c44fa7a7df74b9a5d138537ed
Andrew Cleland <alcleland@gmail.com>
2015-03-19 13:47:07 -0700

    Added deleteNode function

--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,6 @@
 /*  Copyright (c) 2015 Andrew Cleland 
-    Linked List Implementation in Java
+    Integer Linked List Implementation in Java 
+    - Partly based on "Cracking the Coding Interview" by Gayle Laakmann McDowell
 */
 
 public class LinkedList {
@@ -28,6 +29,33 @@ public class LinkedList {
         // Append the new node
         node.next = newNode;
     }
+
+    public LinkedList deleteNode(int nodeDataToDelete) {
+        // If list is an instance of LinkedList, then this method should be called
+        // list = deleteNode(int something). i.e. The return value should replace
+        // the head of the list. 
+
+        LinkedList head = this;
+        
+        // If the head is the thing to delete, move head to next node.
+        if (head.data == nodeDataToDelete) {
+            return head.next;
+        }
+        
+
+        // Iterate through nodes until value matches. 
+        LinkedList node = head;
+        while (node.hasNext()) {
+            if (node.next.data == nodeDataToDelete) {
+                node.next = node.next.next;
+                return head; // Head doesn't change.
+            }
+            node = node.next;
+        }
+        return head;
+    }
+
+
 
     public String toString() {
         String s = "";
